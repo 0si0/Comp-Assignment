ff045bd340215e60227c50298b077bbe27176908
xahn0505 <xahn0505@gmail.com>
2024-11-28 17:14:34 +0900

    Add files via upload

--- module_readscore.py
+++ module_readscore.py
@@ -1,18 +1,12 @@
 import pandas as pd
 
-def get_gender_score(file_path, subject):
-
-    data = pd.read_csv(file_path, encoding='cp949')
-    
-    filtered_data = data[(data['유형'] == subject)]
-    subjects = data['유형'].drop_duplicates().tolist()
-    male_count = filtered_data['남자'].tolist()
-    female_count = filtered_data['여자'].tolist()
-    
-    return subjects, male_count, female_count
-
-
 file_path = '20231231.csv'
+data = pd.read_csv(file_path, encoding='cp949')
 
-male_count, female_count = get_gender_score(file_path, '국어')
+subject = input("과목: ")
+filtered_data = data[data['유형'] == subject]
+
+scores = filtered_data['표준점수'].tolist()
+male_count = filtered_data['남자'].tolist()
+female_count = filtered_data['여자'].tolist()
 
