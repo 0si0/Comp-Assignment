bc3e86bcc2f73fbec5f8b0fd0f6823ed9925ef4f
0si0 <161126882+0si0@users.noreply.github.com>
2024-11-29 19:53:24 +0900

    module_readscore.py

--- module_readscore.py
+++ module_readscore.py
@@ -3,6 +3,10 @@ import pandas as pd
 file_path = '20231231.csv'
 data = pd.read_csv(file_path, encoding='cp949')
 
+subject_category = set()
+for i in data[""]:
+  subject_category.add(i)
+
 subject = input("과목: ")
 filtered_data = data[data['유형'] == subject]
 
