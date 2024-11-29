079dafaa252aaa054b522389be25187c9111c87a
0si0 <161126882+0si0@users.noreply.github.com>
2024-11-29 20:13:17 +0900

    Update module_readscore.py

--- module_readscore.py
+++ module_readscore.py
@@ -7,7 +7,10 @@ subject_category = set()
 for i in data["유형"]:
   subject_category.add(i)
 
+for i in subject_category:
+  print(i)
 
+subject = input("과목을 선택하세요 :  ")
 filtered_data = data[data['유형'] == subject]
 
 
