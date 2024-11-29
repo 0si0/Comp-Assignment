4087ba2bc6a612cb49771670b7208f888835956b
0si0 <161126882+0si0@users.noreply.github.com>
2024-11-29 20:44:28 +0900

    Update module_graph.py

--- module_graph.py
+++ module_graph.py
@@ -1,9 +1,17 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib import rc
+import matplotlib.font_manager as fm
 
-def graphmaker(male_scores, female_scores, year, subject):
 
-  x_values = np.arange(1, len(male_scores) + 1)
+font_path = 'C:/Windows/Fonts/malgun.ttf'  
+
+font_prop = fm.FontProperties(fname=font_path)
+rc('font', family=font_prop.get_name())
+
+def graphmaker(male_scores, female_scores, year, subject, max_score):
+
+  x_values = np.arange(max_score - len(male_scores) + 1, max_score + 1)
 
   plt.plot(x_values, male_scores, label='남자')  
   plt.plot(x_values, female_scores, label='여자')     
@@ -13,8 +21,8 @@ def graphmaker(male_scores, female_scores, year, subject):
   plt.title(f'{year}학년도 수능 {subject}과목분포')  
 
   
-  plt.xlabel('점수')   
-
+  plt.xlabel('표준점수')   
+  plt.xlim(0, max_score)
   
   plt.legend()  
 
